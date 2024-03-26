7176bfee730e2f16a905a518ce18e242ed7bfff3
Mateus Noleto <mateusnoletods@gmail.com>
2024-03-26 14:14:59 -0300

    criado primeiro componente react jsx

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Title from "../src/components/title/Title"
+
+function HomePage (){
+    return(
+        <div>
+            <Title/>
+            Hello word, NextJs + ReactJs
+        </div>
+    )
+}
+
+export default HomePage
