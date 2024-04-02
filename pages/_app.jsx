45732d176965ff33b13aa39c7311b0f4de9a89e5
Mateus Noleto <mateusnoletods@gmail.com>
2024-04-02 08:21:06 -0300

    Impotando fonte e configuracoes do titulo

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,14 @@
+import Head from 'next/head'
 import '../src/style.css'
 
 function App ({Component,pageProps}) {
     return(
-        <Component {...pageProps}/>
+        <>
+         <Head>
+            <title>Titulo padrão</title>
+         </Head>  
+            <Component {...pageProps}/>
+        </>
     )
 }
 export default App
