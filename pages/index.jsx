1fb0867168c1dac33c6d9bbd127615d157c45f18
Mateus Noleto <mateusnoletods@gmail.com>
2024-04-03 21:35:54 -0300

    criando useState

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,35 @@
+import { useState } from "react"
+
 import Title from "../src/components/title/Title"
 import Subtitle from "../src/components/subtitle/Subtitle"
+import Button from "../src/components/button/Button"
 
-function HomePage (){
-    return(
-        <div>
-            <Title text="um texto"/>
-            <p>paragrafo</p>
-            <Title>filho</Title>
-            <p>outro paragrafo</p>
-            <Subtitle text="outro subtitulo!"/>
+function HomePage() {
+    const [click, setClick] = useState(0)
 
-        </div>
+    const handleClick = () => {
+
+        setClick(click + 1)
+    }
+
+
+    return (
+        <>
+            <div>
+                <Title text="um texto" />
+                <p>paragrafo</p>
+                <Title>filho</Title>
+                <p>outro paragrafo</p>
+                <Subtitle text="outro subtitulo!" />
+                <Button
+                    onClick={handleClick}
+                >
+                    Botao teste
+                </Button>
+
+                <p>Cliques: {click}</p>
+            </div>
+        </>
     )
 }
 
